1059f3388c41778941291b1c471b1c6b54d0e3df
Ayas Nasih <ayasnasih@gmail.com>
2022-03-26 17:13:45 +0500

    Remove unused href

--- components/util/IconButton.tsx
+++ components/util/IconButton.tsx
@@ -15,10 +15,7 @@ export const IconButton = ({ label, icon }: IconButtonProps) => {
       <HeartIcon fill="transparent" width="16" height="16" stroke="#0FCCCE" />
     )
   return (
-    <a
-      className="inline-block rounded-md bg-lf-grey bg-opacity-20 py-2 pl-3 pr-5 text-sm hover:bg-opacity-30"
-      href="Download"
-    >
+    <a className="inline-block rounded-md bg-lf-grey bg-opacity-20 py-2 pl-3 pr-5 text-sm hover:bg-opacity-30">
       {renderIcon}
       {label}
     </a>
