5df52fa18227e98f186ae465d99179949206cecf
Ayas Nasih <ayasnasih@gmail.com>
2022-03-26 15:11:50 +0500

    Implement fetching data from gql

--- components/animationList.tsx
+++ components/animationList.tsx
@@ -5,74 +5,30 @@ import LottiePreviewCard from './animationList/LottiePreviewCard'
 import SortByDropdown from './animationList/SortByDropdown'
 import Subnav from './animationList/Subnav'
 import { Lottie } from './util/LottieTypes'
+import { gql, useQuery } from '@apollo/client'
 
-const lotties: Lottie[] = [
-  {
-    id: 1,
-    author: {
-      name: 'Anant Mishra',
-      avatar: 'https://assets2.lottiefiles.com/avatars/300_61bc7ad399637.jpg',
-    },
-    assetUrl: 'https://assets2.lottiefiles.com/packages/lf20_qsrqwtwr.json',
-    downloads: '0',
-    likes: '0',
-    title: 'Sample title',
-    description: 'Sample description',
-    createdAt: '',
-  },
-  {
-    id: 2,
-    author: {
-      name: 'Alysson Franklin',
-    },
-    assetUrl: 'https://assets5.lottiefiles.com/packages/lf20_xim69oha.json',
-    downloads: '34',
-    likes: '6',
-    title: 'Test Raffle',
-    description: 'test',
-    createdAt: '',
-  },
-  {
-    id: 3,
-    author: {
-      name: 'Diego Zito',
-      avatar: 'https://assets5.lottiefiles.com/avatars/300_611b9711cbf0c.jpg',
-    },
-    assetUrl: 'https://assets5.lottiefiles.com/packages/lf20_dsopa1mi.json',
-    downloads: '0',
-    likes: '0',
-    title: 'Test',
-    description: 'Test',
-    createdAt: '',
-  },
-  {
-    id: 4,
-    author: {
-      name: 'mayank Nader',
-      avatar: 'https://assets5.lottiefiles.com/avatars/300_620d0f4b7bc24.jpg',
-    },
-    assetUrl: 'https://assets5.lottiefiles.com/packages/lf20_l11so9rv.json',
-    downloads: '0',
-    likes: '1',
-    title: 'Testing',
-    description: 'Test',
-    createdAt: '',
-  },
-  {
-    id: 5,
-    author: {
-      name: 'Sarf Pearl',
-    },
-    assetUrl: 'https://assets5.lottiefiles.com/packages/lf20_w6v4gptz.json',
-    downloads: '42',
-    likes: '3',
-    title: 'Test Load',
-    description: 'Test',
-    createdAt: '',
-  },
-]
+const AllAnimationsQuery = gql`
+  query Query {
+    lotties {
+      id
+      title
+      description
+      assetUrl
+      downloads
+      likes
+      user {
+        name
+        avatar
+      }
+    }
+  }
+`
 
 const AnimationList = ({ title }: { title: string }) => {
+  const { data, error, loading } = useQuery(AllAnimationsQuery)
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Oops, something went wrong - {error.message}</p>
+
   return (
     <FluidLayout>
       <Head>
@@ -107,7 +63,7 @@ const AnimationList = ({ title }: { title: string }) => {
       </section>
       <section className="bg-gray-100 py-6">
         <div className="container mx-auto grid grid-cols-4 gap-6">
-          {lotties.map((lottie) => (
+          {data.lotties.map((lottie) => (
             <LottiePreviewCard key={lottie.id} lottie={lottie} />
           ))}
         </div>
