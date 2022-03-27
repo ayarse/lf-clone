f7b699f0c3af5e4f31c8fb72bad8b437a96d717d
Ayas Nasih <ayasnasih@gmail.com>
2022-03-27 15:49:34 +0500

    Create featured and popular pages

--- pages/featured.tsx
+++ pages/featured.tsx
@@ -1,9 +1,43 @@
+import { gql, useQuery } from '@apollo/client'
 import type { NextPage } from 'next'
 
 import AnimationList from '../components/AnimationList'
 
+const FeaturedLottiesQuery = gql`
+  query Tags {
+    tags(name: "featured") {
+      name
+      id
+      lotties {
+        id
+        title
+        description
+        assetUrl
+        downloads
+        likes
+        user {
+          name
+          avatar
+        }
+      }
+    }
+  }
+`
+
 const FeaturedAnimations: NextPage = () => {
-  return <AnimationList title="Featured Animations" />
+  // const [d, setD] = useState()
+
+  const { data, error, loading } = useQuery(FeaturedLottiesQuery)
+
+  return (
+    <AnimationList
+      lotties={data?.tags?.lotties}
+      error={error}
+      loading={loading}
+      mode="browse"
+      title="Featured Animations"
+    />
+  )
 }
 
 export default FeaturedAnimations
--- pages/popular.tsx
+++ pages/popular.tsx
@@ -1,9 +1,38 @@
+import { gql, useQuery } from '@apollo/client'
 import type { NextPage } from 'next'
 
 import AnimationList from '../components/AnimationList'
 
+const PopularLottiesQuery = gql`
+  query Query {
+    lotties(sortBy: "downloads") {
+      id
+      title
+      description
+      assetUrl
+      downloads
+      likes
+      user {
+        name
+        avatar
+      }
+    }
+  }
+`
+
 const PopularAnimations: NextPage = () => {
-  return <AnimationList title="Popular Animations" />
+  const { data, error, loading } = useQuery(PopularLottiesQuery)
+
+  return (
+    <AnimationList
+      mode="browse"
+      lotties={data?.lotties}
+      error={error}
+      loading={loading}
+      query={PopularLottiesQuery}
+      title="Popular Animations"
+    />
+  )
 }
 
 export default PopularAnimations
