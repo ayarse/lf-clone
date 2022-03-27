9c152893e59cf3c13ad352a59c7cf1b5ec6641d5
Ayas Nasih <ayasnasih@gmail.com>
2022-03-27 15:49:19 +0500

    Create search page

--- components/searchbox.tsx
+++ components/searchbox.tsx
@@ -1,8 +1,31 @@
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+
+const preventDefault = (f) => (e) => {
+  e.preventDefault()
+  f(e)
+}
+
 const SearchBox = () => {
+  const router = useRouter()
+  const [query, setQuery] = useState(router.query.q ?? '')
+
+  const handleParam = (setValue) => (e) => setValue(e.target.value)
+
+  const handleSubmit = preventDefault(() => {
+    router.push({
+      pathname: '/search',
+      query: { q: query },
+    })
+  })
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
         type="text"
+        name="q"
+        value={query}
+        onChange={handleParam(setQuery)}
         placeholder="Search for animations"
         className="bg-search-icon w-4/6 rounded-lg border border-gray-200 p-2 pl-8 text-sm shadow-md shadow-gray-100 transition-all duration-200 hover:border-lf-teal hover:bg-lf-teal hover:text-white hover:placeholder:text-teal-100 focus:outline-none"
       />
--- pages/search.tsx
+++ pages/search.tsx
@@ -1,9 +1,41 @@
+import { gql, useQuery } from '@apollo/client'
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
 
 import AnimationList from '../components/AnimationList'
 
+const AnimationSearchQuery = (query) => gql`
+  query Query {
+    search(query: "${query}") {
+      id
+      title
+      description
+      assetUrl
+      downloads
+      likes
+      user {
+        name
+        avatar
+      }
+    }
+  }
+`
+
 const Search: NextPage = () => {
-  return <AnimationList title='Search Results for "asd"' />
+  const router = useRouter()
+  const { data, error, loading } = useQuery(
+    AnimationSearchQuery(router.query.q)
+  )
+
+  return (
+    <AnimationList
+      mode="search"
+      lotties={data?.search}
+      error={error}
+      loading={loading}
+      title={`Search Results for "${router.query.q}"`}
+    />
+  )
 }
 
 export default Search
