13c3f31bec19b959a2aa44734a9aeecada373534
Ayas Nasih <ayasnasih@gmail.com>
2022-03-28 15:52:37 +0500

    Fix sorting dropdown

--- components/animationList/LfDropdown.tsx
+++ components/animationList/LfDropdown.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from 'react'
+import { ReactNode, useEffect, useRef, useState } from 'react'
 import { ChevronDownIcon } from '../util'
 
 type LfDropdownProps = {
@@ -8,18 +8,21 @@ type LfDropdownProps = {
 
 const LfDropdown = ({ title, children }: LfDropdownProps) => {
   const [open, setOpen] = useState(false)
-  useEffect(() => {
-    document.addEventListener('mousedown', () => {
-      setOpen(false)
-    })
-  })
+  const dropdownRef = useRef(null)
+  const [listening, setListening] = useState(false)
+
+  useEffect(
+    listenForOutsideClicks(listening, setListening, dropdownRef, setOpen)
+  )
+
   function clickDropdown() {
     setOpen(!open)
   }
+
   return (
-    <div className="relative">
+    <div ref={dropdownRef} className="relative">
       <div
-        className={`text-lf-grey-darkest cursor-pointer rounded-md border border-gray-300 px-3 py-1 text-sm ${
+        className={`cursor-pointer rounded-md border border-gray-300 px-3 py-1 text-sm text-lf-grey-darkest ${
           open ? 'bg-gray-100' : 'bg-white'
         }`}
         onClick={clickDropdown}
@@ -39,3 +42,22 @@ const LfDropdown = ({ title, children }: LfDropdownProps) => {
 }
 
 export default LfDropdown
+
+function listenForOutsideClicks(
+  listening,
+  setListening,
+  dropdownRef,
+  setIsOpen
+) {
+  return () => {
+    if (listening) return
+    if (!dropdownRef.current) return
+    setListening(true)
+    ;[`click`, `touchstart`].forEach((_type) => {
+      document.addEventListener(`click`, (evt) => {
+        if (dropdownRef.current?.contains(evt.target)) return
+        setIsOpen(false)
+      })
+    })
+  }
+}
--- components/animationList/SortByDropdown.tsx
+++ components/animationList/SortByDropdown.tsx
@@ -1,17 +1,24 @@
+import Link from 'next/link'
 import LfDropdown from './LfDropdown'
 
 const SortByDropdown = () => {
   return (
     <LfDropdown title="Sort By">
-      <a className="-mx-3 block cursor-pointer px-12 py-2 text-xs hover:bg-gray-100 hover:text-lf-teal-600">
-        Popular
-      </a>
-      <a className="-mx-3 block cursor-pointer px-12 py-2 text-xs hover:bg-gray-100 hover:text-lf-teal-600">
-        Oldest
-      </a>
-      <a className="-mx-3 block cursor-pointer px-12 py-2 text-xs hover:bg-gray-100 hover:text-lf-teal-600">
-        Newest
-      </a>
+      <Link href="/popular">
+        <a className="-mx-3 block cursor-pointer px-12 py-2 text-xs hover:bg-gray-100 hover:text-lf-teal-600">
+          Popular
+        </a>
+      </Link>
+      <Link href="/browse?order=asc">
+        <a className="-mx-3 block cursor-pointer px-12 py-2 text-xs hover:bg-gray-100 hover:text-lf-teal-600">
+          Oldest
+        </a>
+      </Link>
+      <Link href="/browse?order=desc">
+        <a className="-mx-3 block cursor-pointer px-12 py-2 text-xs hover:bg-gray-100 hover:text-lf-teal-600">
+          Newest
+        </a>
+      </Link>
     </LfDropdown>
   )
 }
